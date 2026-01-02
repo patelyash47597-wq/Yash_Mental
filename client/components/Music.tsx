5fb21ce077268b8059ef7d90905badc51c1c1243
Patelyash <patelyash49597@gmail.com>
2026-01-02 22:38:17 +0530

    final call

--- client/components/Music.tsx
+++ client/components/Music.tsx
@@ -25,7 +25,7 @@ const MusicAssistant: React.FC = () => {
 
     if (!text) return alert("Please describe how you're feeling!");
 
-    const res = await fetch("https://yashatmental.onrender.com/meditation/detect_mood", {
+    const res = await fetch("https://yashatmental.onrender.com/detect_mood", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text })
